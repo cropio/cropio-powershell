d038a9912e9f825a6c7152e19ee188e59519e024
Oleksii Leonov <mail@aleksejleonov.com>
2021-10-19 18:10:23 +0300

    Add example for mass updating properties for objects with specific name (#4)

--- scripts/update_properties/update_properties_with_specific_name.ps1
+++ scripts/update_properties/update_properties_with_specific_name.ps1
@@ -8,8 +8,9 @@
 ##################
 $userApiToken="USER_API_TOKEN"
 $apiEndpoint="api/v3/additional_objects"
-$filter="object_type=Farm"
-$dataToUpdate='{ "data": { "polygon_color": "#007700", "polygon_opacity": 0.5 } }'
+$filter="geometry_type=line"
+$dataToUpdate='{ "data": { "line_color": "#007700" } }'
+$nameStartsWith='farm'
 ##################
 
 # Preparations
@@ -52,8 +53,20 @@ Write-Warning "Start updating records?" -WarningAction Inquire
 
 # Update each record.
 foreach ($id in $idsToUpdate) {
+    Write-Host ""
+    Write-Host "ID: $id"
+
     $urlBuilder = new-object System.UriBuilder($baseUrl)
     $urlBuilder.Path = $urlBuilder.Path + "/$id"
     $url = $urlBuilder.ToString()
-    Invoke-RestMethod -Method Put -ContentType 'application/json' -Headers $authHeader -Uri $url -Body $dataToUpdate
+
+    # Get object
+    $object = (Invoke-RestMethod -Method Get -ContentType 'application/json' -Headers $authHeader -Uri $url).data
+
+    if($object.name.StartsWith($nameStartsWith)) {
+        Write-Host 'Updating record...'
+        Invoke-RestMethod -Method Put -ContentType 'application/json' -Headers $authHeader -Uri $url -Body $dataToUpdate
+   } else {
+        Write-Host "Skip (name doesn't match)."
+   }
 }
