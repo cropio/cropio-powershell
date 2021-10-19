30483d9988ed7f49e754b5de220b01550d377049
Oleksii Leonov <mail@aleksejleonov.com>
2021-10-19 13:42:54 +0300

    More info and ask confirmation got update_properties script (#3)

--- scripts/update_properties/update_properties.ps1
+++ scripts/update_properties/update_properties.ps1
@@ -6,12 +6,15 @@
 ##################
 #### Settings ####
 ##################
-$userApiToken="USER_TOKEN"
-$baseUrl= "https://operations.cropwise.com/api/v3/additional_objects"
+$userApiToken="USER_API_TOKEN"
+$apiEndpoint="api/v3/additional_objects"
 $filter="object_type=Farm"
 $dataToUpdate='{ "data": { "polygon_color": "#007700", "polygon_opacity": 0.5 } }'
 ##################
 
+# Preparations
+$apiUrl = "https://operations.cropwise.com/"
+$baseUrl= $apiUrl + $apiEndpoint
 $authHeader=@{"X-User-Api-Token"=$userApiToken}
 
 # Get IDs to update.
@@ -21,7 +24,33 @@ $urlBuilder.Query = $filter
 $url = $urlBuilder.ToString()
 $idsToUpdate = (Invoke-RestMethod -Method Get -ContentType 'application/json' -Headers $authHeader -Uri $url).data
 
-# Update each record
+# Get number of available records.
+$urlBuilder = new-object System.UriBuilder($baseUrl)
+$urlBuilder.Path = $urlBuilder.Path + '/count'
+$url = $urlBuilder.ToString()
+$availableRecordsCount = (Invoke-RestMethod -Method Get -ContentType 'application/json' -Headers $authHeader -Uri $url).data
+
+# Get number of fitlered records.
+$filteredeRecordsCount = $idsToUpdate.Count;
+
+# Show information of records that would be updated.
+Write-Host "API endpoint: " -NoNewline
+Write-Host $apiEndpoint -ForegroundColor DarkGreen
+Write-Host "Filter: " -NoNewline
+Write-Host $filter -ForegroundColor DarkGreen
+Write-Host "Data: " -NoNewline
+Write-Host $dataToUpdate -ForegroundColor DarkGreen
+Write-Host ""
+Write-Host "Total available records: " -NoNewline
+Write-Host $availableRecordsCount -ForegroundColor DarkYellow
+Write-Host "Records to update (filtered): " -NoNewline
+Write-Host $filteredeRecordsCount -ForegroundColor DarkYellow
+
+# Ask to start update.
+Write-Host ""
+Write-Warning "Start updating records?" -WarningAction Inquire
+
+# Update each record.
 foreach ($id in $idsToUpdate) {
     $urlBuilder = new-object System.UriBuilder($baseUrl)
     $urlBuilder.Path = $urlBuilder.Path + "/$id"
